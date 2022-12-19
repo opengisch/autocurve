e13c9eb86369d3ad771a5a41b6b9b829a8dc9e07
Olivier Dalang <olivier.dalang@gmail.com>
2022-12-19 13:53:06 +0100

    tests: add autocurve test

--- autocurve_tests/tests_integration.py
+++ autocurve_tests/tests_integration.py
@@ -54,16 +54,18 @@ class IntegrationTest(unittest.TestCase):
         vl.endEditCommand()
         vl.commitChanges()
 
+    def _vtx_at_angle(self, angle: int) -> str:
+        """Returns a vertex at given angle in WKT notation"""
+        return f"{math.cos(math.radians(angle))} {math.sin(math.radians(angle))}"
+
     def test_center_points(self):
         # Disable the actions
         plugins["autocurve"].auto_curve_action.setChecked(False)
         plugins["autocurve"].harmonize_arcs_action.setChecked(False)
 
         # Create two shapes that have a common arc with a different center point
-        CP_A = [math.cos(math.radians(30)), math.sin(math.radians(30))]
-        CP_B = [math.cos(math.radians(60)), math.sin(math.radians(60))]
-        WKT_A = f"CURVEPOLYGON( COMPOUNDCURVE( (0 0, 0 1), CIRCULARSTRING(0 1, {CP_A[0]} {CP_A[1]}, 1 0), (1 0, 0 0) ) )"
-        WKT_B = f"CURVEPOLYGON( COMPOUNDCURVE( (1 1, 0 1), CIRCULARSTRING(0 1, {CP_B[0]} {CP_B[1]}, 1 0), (1 0, 1 1) ) )"
+        WKT_A = f"CURVEPOLYGON( COMPOUNDCURVE( (0 0, 0 1), CIRCULARSTRING(0 1, {self._vtx_at_angle(30)}, 1 0), (1 0, 0 0) ) )"
+        WKT_B = f"CURVEPOLYGON( COMPOUNDCURVE( (1 1, 0 1), CIRCULARSTRING(0 1, {self._vtx_at_angle(60)}, 1 0), (1 0, 1 1) ) )"
 
         # Add them to a layer
         vl = QgsVectorLayer(f"curvepolygon?crs=epsg:2056", "temp", "memory")
@@ -108,3 +110,46 @@ class IntegrationTest(unittest.TestCase):
             vl.getFeature(1).geometry().vertexAt(2),
             vl.getFeature(2).geometry().vertexAt(2),
         )
+
+    def test_autocurve(self):
+        # Disable the actions
+        plugins["autocurve"].auto_curve_action.setChecked(False)
+        plugins["autocurve"].harmonize_arcs_action.setChecked(False)
+
+        # Create a segmented shape
+        SEGMENTED_ARC = ",".join(self._vtx_at_angle(a) for a in range(0, 90, 1))
+        WKT = f"POLYGON(( 0 0, {SEGMENTED_ARC}, 0 0 ))"
+
+        # Add them to a layer
+        vl = QgsVectorLayer(f"curvepolygon?crs=epsg:2056", "temp", "memory")
+        feat = QgsFeature()
+        feat.setGeometry(QgsGeometry.fromWkt(WKT).forceRHR())
+        vl.dataProvider().addFeature(feat)
+
+        vl.loadNamedStyle(str(Path(__file__).parent / "curvepolygon.qml"))
+        QgsProject.instance().addMapLayer(vl)
+
+        self.feedback()
+
+        # Select the layer
+        iface.setActiveLayer(vl)
+
+        # The arc is still segmented
+        self.assertGreater(vl.getFeature(1).geometry().constGet().nCoordinates(), 5)
+
+        # Edit a feature with harmonize_arcs disabled
+        self._move_vertex(vl, feat_id=1, vtx_id=0, x=-0.1, y=-0.1)
+
+        self.feedback()
+
+        # The arc should still be segmented
+        self.assertGreater(vl.getFeature(1).geometry().constGet().nCoordinates(), 5)
+
+        # Edit a feature with harmonize_arcs enabled
+        plugins["autocurve"].auto_curve_action.setChecked(True)
+        self._move_vertex(vl, feat_id=1, vtx_id=0, x=-0.2, y=-0.2)
+
+        self.feedback()
+
+        # The arc should now be curvified
+        self.assertEqual(vl.getFeature(1).geometry().constGet().nCoordinates(), 5)
